a72b48a7fa64b7e99c6b8c85c9fa603a3853bb7f
Joshua Brandes <47532061+joshuabrandes@users.noreply.github.com>
2024-06-17 17:08:25 +0200

    adds run shell scripts

---
+++ run_mac.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Überprüfen, ob venv existiert, und erstellen, falls nicht
+if [ ! -d "venv" ]; then
+    echo "Erstelle virtuelle Umgebung..."
+    python3 -m venv venv
+fi
+
+# Aktivieren der virtuellen Umgebung
+echo "Aktiviere virtuelle Umgebung..."
+source venv/bin/activate
+
+# Ausführen des Python-Skripts
+echo "Führe Python-Skript aus..."
+python path/to/your_script.py
---
+++ run_windows.bat
@@ -0,0 +1,13 @@
+REM Überprüfen, ob venv existiert, und erstellen, falls nicht
+if not exist venv (
+    echo Erstelle virtuelle Umgebung...
+    python -m venv venv
+)
+
+REM Aktivieren der virtuellen Umgebung
+echo Aktiviere virtuelle Umgebung...
+call venv\Scripts\activate.bat
+
+REM Ausführen des Python-Skripts
+echo Führe Python-Skript aus...
+python .\program.py
