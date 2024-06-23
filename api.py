73fb49c29058fc59afb563901b533f1dea51e313
Joshua Brandes <47532061+joshuabrandes@users.noreply.github.com>
2024-06-23 20:02:02 +0200

    adds sqlite db for storing generated images

--- api.py
+++ api.py
@@ -1,12 +1,28 @@
 import io
 import base64
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from diffuser import Diffuser, StableDiffusionModel
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/images.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 diffuser = {
     StableDiffusionModel.STABLE_DIFFUSION_XL.value: Diffuser(),
 }
+
+
+class GeneratedImage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    image = db.Column(db.LargeBinary, nullable=False)
+    prompt = db.Column(db.String(750), nullable=False)
+    negative_prompt = db.Column(db.String(750), nullable=False)
+    timestamp = db.Column(db.DateTime, server_default=db.func.now())
+
+    def __repr__(self):
+        return f'<GeneratedImage {self.id}>'
+
 
 def convert_images(images, image_type="PNG", compression_ratio=0.9):
     converted_images = []
@@ -22,6 +38,7 @@ def convert_images(images, image_type="PNG", compression_ratio=0.9):
         converted_images.append(image_base64)
 
     return converted_images
+
 
 @app.route("/generate", methods=["POST"])
 def generate_image():
@@ -56,4 +73,28 @@ def generate_image():
 
     converted_images = convert_images(images)
 
-    return jsonify({"images": converted_images})
+    saved_images = []
+    # Save the images to the database
+    for image in converted_images:
+        decoded_image = base64.b64decode(image)
+        new_image = GeneratedImage(image=decoded_image, prompt=prompt, negative_prompt=negative_prompt)
+        saved_images.append(new_image)
+        db.session.add(new_image)
+
+    db.session.commit()
+
+    # build pairs of image id and base64 image with field names id and image
+    result_pairs = [{"id": image.id, "image": image_base64} for image, image_base64 in
+                    zip(saved_images, converted_images)]
+
+    return jsonify(result_pairs)
+
+
+@app.route("/images/<int:image_id>", methods=["GET"])
+def get_image(image_id):
+    image = GeneratedImage.query.get(image_id)
+    if image is None:
+        return jsonify({"error": "Image not found"}), 404
+
+    image_base64 = base64.b64encode(image.image).decode('utf-8')
+    return jsonify({"id": image.id, "image": image_base64, "prompt": image.prompt})
