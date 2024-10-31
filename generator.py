ceb7424599ee3169cca2e7e4aa33ea877ff3a5b1
Joshua Brandes <47532061+joshuabrandes@users.noreply.github.com>
2024-10-31 21:41:41 +0100

    adds Stable Diffusion 3.5 Medium

--- generator.py
+++ generator.py
@@ -15,6 +15,7 @@ class DiffusionModel(Enum):
     STABLE_DIFFUSION_1_5 = "runwayml/stable-diffusion-v1-5"
     STABLE_DIFFUSION_XL = "stabilityai/stable-diffusion-xl-base-1.0"
     STABLE_DIFFUSION_3_MEDIUM = "stabilityai/stable-diffusion-3-medium"
+    STABLE_DIFFUSION_3_5_MEDIUM = "stabilityai/stable-diffusion-3.5-medium"
     FLUX_1_SCHNELL = "black-forest-labs/FLUX.1-schnell"
 
 
