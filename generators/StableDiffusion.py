59a4fbc9043e0a6ad044764a366deeabcb9dac0d
joshuabrandes <47532061+joshuabrandes@users.noreply.github.com>
2024-11-19 09:07:56 +0100

    SD 3.5 working now (requires 20+ GB of available VRAM)

--- generators/StableDiffusion.py
+++ generators/StableDiffusion.py
@@ -1,7 +1,7 @@
 import time
 
 import torch
-from diffusers import DiffusionPipeline
+from diffusers import DiffusionPipeline, BitsAndBytesConfig
 from transformers import T5Tokenizer, T5EncoderModel
 
 from models import DiffusionModel
@@ -9,12 +9,23 @@ from models import DiffusionModel
 
 class StableDiffusion:
     def __init__(self, model_id: str, device: str = "cpu"):
-        self.pipe = DiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float32, use_safetensors=True)
+        # float16 if CUDA is available, otherwise float32
+        dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+        self.pipe = DiffusionPipeline.from_pretrained(model_id, torch_dtype=dtype, use_safetensors=True)
 
         # set Text Encoder to T5_xxl for SD 3.5
         if model_id == DiffusionModel.STABLE_DIFFUSION_3_5_MEDIUM.value:
-            t5_tokenizer = T5Tokenizer.from_pretrained("t5-xxl")
-            t5_encoder = T5EncoderModel.from_pretrained("t5-xxl")
+            t5_encoder = T5EncoderModel.from_pretrained(
+                "stabilityai/stable-diffusion-3.5-medium",
+                subfolder="text_encoder_3",
+                device_map="auto",
+            )
+
+            # Tokenizer laden
+            t5_tokenizer = T5Tokenizer.from_pretrained(
+                "stabilityai/stable-diffusion-3.5-medium",
+                subfolder="tokenizer_3",
+            )
 
             self.pipe.tokenizer = t5_tokenizer
             self.pipe.text_encoder = t5_encoder
